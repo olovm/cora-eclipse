1858929513486b938f8f1b20294a20109e6a1d93
olov <olov@mckie.se>
2017-01-02 18:08:14 +0100

    using useradd, adding entrypoint.sh

---
+++ docker/entrypoint.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+git clone https://github.com/olovm/cora-eclipse.git ~/workspace/cora-eclipse
+
+chmod +x ~/workspace/cora-eclipse/docker/setupProjects.sh
+
+.~/workspace/cora-eclipse/docker/setupProjects.sh
+
+~/eclipse-installer/eclipse-inst
