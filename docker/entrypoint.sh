3b1723ae640feadf1805713ecd67be6a9ca38a6e
olov <olov@mckie.se>
2017-01-02 19:18:43 +0100

    updating entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,6 @@ git clone https://github.com/olovm/cora-eclipse.git ~/workspace/cora-eclipse
 
 chmod +x ~/workspace/cora-eclipse/docker/setupProjects.sh
 
-.~/workspace/cora-eclipse/docker/setupProjects.sh
+~/workspace/cora-eclipse/docker/setupProjects.sh
 
 ~/eclipse-installer/eclipse-inst
