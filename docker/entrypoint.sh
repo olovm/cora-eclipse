3b37b875d569e12c551b9748a286c7f666cfce64
olov <olov@e7fd395d729d>
2017-01-11 19:46:24 +0000

    addng nmp install karma to entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,4 +6,7 @@ chmod +x ~/workspace/cora-eclipse/docker/setupProjects.sh
 
 ~/workspace/cora-eclipse/docker/setupProjects.sh
 
+cd ~/workspace/cora-jsclient/
+npm install karma karma-chrome-launcher karma-firefox-launcher karma-qunit karma-coverage karma-html-reporter --save-dev
+
 SWT_GTK3=0  ~/eclipse-installer/eclipse-inst
