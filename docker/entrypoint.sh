00174018bfa94827b74166d677686b0a2d825221
olov <olov@e7fd395d729d>
2017-01-12 05:54:12 +0000

    change to start entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,22 @@
 #! /bin/bash
 
-git clone https://github.com/olovm/cora-eclipse.git ~/workspace/cora-eclipse
+firstRun(){
+	git clone https://github.com/olovm/cora-eclipse.git ~/workspace/cora-eclipse
+	
+	chmod +x ~/workspace/cora-eclipse/docker/setupProjects.sh
+	~/workspace/cora-eclipse/docker/setupProjects.sh
+	
+	cd ~/workspace/cora-jsclient/
+	npm install karma karma-chrome-launcher karma-firefox-launcher karma-qunit karma-coverage karma-html-reporter --save-dev
+	
+	#SWT_GTK3=0  ~/eclipse-installer/eclipse-inst
+	~/eclipse-installer/eclipse-inst
+}
 
-chmod +x ~/workspace/cora-eclipse/docker/setupProjects.sh
+export SWT_GTK3=0
 
-~/workspace/cora-eclipse/docker/setupProjects.sh
-
-cd ~/workspace/cora-jsclient/
-npm install karma karma-chrome-launcher karma-firefox-launcher karma-qunit karma-coverage karma-html-reporter --save-dev
-
-SWT_GTK3=0  ~/eclipse-installer/eclipse-inst
+if [ ! -d "~/workspace/cora-jsclient" ]; then
+  	firstRun
+else
+	~/eclipse/eclipseforcora/eclipse
+fi
