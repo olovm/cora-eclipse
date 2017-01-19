bc5aab43bb8e00daef43751a81d9d44944525f44
olov <olov@fe93bd3d8bb8>
2017-01-19 21:02:08 +0000

    minor update

--- startEclipseForCora.sh
+++ startEclipseForCora.sh
@@ -7,10 +7,10 @@ if [ ! $USER ]; then
 else
 	docker run --rm -ti --privileged -e DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
-	-v /mnt/depot/eclipseForCora/workspace:/home/$USER/workspace \
-	-v /mnt/depot/eclipseForCora/eclipse:/home/$USER/eclipse \
-	-v /mnt/depot/eclipseP2:/home/$USER/.p2 \
-	-v /mnt/depot/eclipseForCora/m2:/home/$USER/.m2 \
+	-v ../workspace:/home/$USER/workspace \
+	-v ../eclipse:/home/$USER/eclipse \
+	-v ../m2:/home/$USER/.m2 \
+	-v ../../eclipseP2:/home/$USER/.p2 \
 	--env user=$USER -p 8080:8080 -p 9876:9876 -p 8090:8090 --name eclipseforcora1 eclipseforcora1
 	
 fi
