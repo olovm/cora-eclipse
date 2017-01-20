df6ef59e3ef0f1c37e7f5331f1bbbfda4055e755
olov <olov@24c906d2d418>
2017-01-20 13:37:46 +0000

    adding setlupDirectoriesAndScripts

--- setupDirectoriesAndScriptsForEclipseForCora.sh
+++ setupDirectoriesAndScriptsForEclipseForCora.sh
@@ -21,7 +21,7 @@ changeAndCopyScripts(){
 	
 	cp $BASEDIR/startEclipseForCora.sh $PARENTDIR/eclipseForCora/
 	sed -i "s|INSTALLDIR|$INSTALLDIR|g" $PARENTDIR/eclipseForCora/startEclipseForCora.sh
-	sed -i "s|TOPDIR|$TOPDIR|g" $PARENTDIR/eclipseForCora/startEclipseForCora.sh
+	sed -i "s|PARENTDIR|$PARENTDIR|g" $PARENTDIR/eclipseForCora/startEclipseForCora.sh
 	
 	cp $BASEDIR/buildEclipseForCora.sh $PARENTDIR/eclipseForCora/
 	sed -i "s|INSTALLDIR|$INSTALLDIR|g" $PARENTDIR/eclipseForCora/buildEclipseForCora.sh
--- startEclipseForCora.sh
+++ startEclipseForCora.sh
@@ -12,7 +12,7 @@ else
 	-v INSTALLDIR/workspace:/home/$USER/workspace \
 	-v INSTALLDIR/eclipse:/home/$USER/eclipse \
 	-v INSTALLDIR/m2:/home/$USER/.m2 \
-	-v TOPDIR/eclipseP2:/home/$USER/.p2 \
+	-v PARENTDIR/eclipseP2:/home/$USER/.p2 \
 	--env user=$USER -p 8080:8080 -p 9876:9876 -p 8090:8090 --name eclipseforcora1 eclipseforcora1
 	
 fi
