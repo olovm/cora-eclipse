83b6c30c6dfebde8b89f1076fb72130fb3a28294
olov <olov@fdf002984394>
2017-01-25 19:55:20 +0000

    choice of origin when cloning repositories

--- docker/setupProjects.sh
+++ docker/setupProjects.sh
@@ -8,68 +8,83 @@ else
 	export user
 fi
 
-#prevent git asking for username password if repo is missing
-export GIT_ASKPASS="/bin/true"
+importProjectListing() {
+	#import projectListing
+	.  ~/workspace/cora-eclipse/development/projectListing.sh
+}
 
+preventGitAskingForUsernameAndPasswordIfRepoIsMissing() {
+	#prevent git asking for username password if repo is missing
+	export GIT_ASKPASS="/bin/true"
+}
 
-addRepro() {
-	local name=$1
+addOtherRemotes(){
+	local repositoryName=$1
+	for otherRepo in $otherRepos; do
+		git remote add github-$otherRepo https://github.com/$otherRepo/$repositoryName.git
+	done
+}
+
+cloneRepoAndAddRemotes() {
+	local repositoryName=$1
 	cd /home/$user/workspace
-	git clone https://github.com/olovm/$name.git
-	cd /home/$user/workspace/$name
-	git remote add github-lsu https://github.com/lsu-ub-uu/$name.git
-	git remote add github-maddekenn https://github.com/maddekenn/$name.git
+	git clone https://github.com/olovm/$repositoryName.git
+	cd /home/$user/workspace/$repositoryName
+	#git remote add github-lsu https://github.com/lsu-ub-uu/$repositoryName.git
+	#git remote add github-maddekenn https://github.com/maddekenn/$repositoryName.git
+	addOtherRemotes $repositoryName
 	git fetch --all
 	cd /home/$user/workspace
 }
 
-setBasePathToPointToBasicStorageWorkspaceDirectory(){
+addAllRepositories() {
+	for PROJECT in $ALL; do
+		cloneRepoAndAddRemotes $PROJECT
+	done
+}
+
+setBasePathToPointToBasicStorageWorkspaceDirectoryInTomcatContextXml(){
 	sed -i "s|WORKSPACEDIR|/home/$user/workspace|g" "/home/$user/workspace/cora-eclipse/oomph/Servers/Tomcat v8.5 Server at localhost-config/context.xml"
 }
 
-#import projectListing
-.  ~/workspace/cora-eclipse/development/projectListing.sh
+chooseRepo(){
+	echo ""
+	echo "Please choose the remote you want to use as origin or enter a different one, "
+	echo "where the cora projects have been cloned."
+	echo "1. https://github.com/lsu-ub-uu/"
+	echo "2. https://github.com/olovm/"
+	echo "3. https://github.com/maddekenn/"
+	echo "Choose 1, 2, 3 or enter your own base url to clone as origin. (eg. https://github.com/olovm/)"
+	read -p "For origin, use? " userchoice
+	case "$userchoice" in
+	        1)
+				echo "You choose: $userchoice 1111"
+	            originRepo="https://github.com/lsu-ub-uu/"
+	            otherRepos="olomv maddekenn"
+	            ;;
+	        2)
+				echo "You choose: $userchoice 2222"
+	            originRepo="https://github.com/olovm/"
+	            otherRepos="lsu-ub-uu maddekenn"
+	            ;;
+	        3)
+				echo "You choose: $userchoice 3333"
+	            originRepo="https://github.com/maddekenn/"
+	            otherRepos="lsu-ub-uu olomv"
+	            ;;
+	        *)
+				echo "You choose: $userchoice other"
+	            originRepo="$userchoice"
+	            otherRepos="lsu-ub-uu olomv maddekenn"
+	esac
+	
+	echo "Origin choosen as: $originRepo"
+	echo "Others remotes will be: $otherRepos"
+}
 
-for PROJECT in $ALL; do
-	addRepro $PROJECT
-done
-
-#addRepro "cora-eclipse"
-setBasePathToPointToBasicStorageWorkspaceDirectory
-
-#TIER0
-#addRepro "cora-parent"
-
-#TIER1
-#addRepro "cora-json"
-#addRepro "cora-httphandler"
-#addRepro "cora-userpicker"
-
-#TIER2
-#addRepro "cora-beefeater"
-#addRepro "cora-bookkeeper"
-#addRepro "cora-gatekeeper"
-
-#TIER3
-#addRepro "cora-spider"
-#addRepro "cora-gatekeepertokenprovider"
-
-
-#TIER4
-#addRepro "cora-basicstorage"
-#addRepro "cora-gatekeeperclient"
-#addRepro "cora-therest"
-#addRepro "cora-apptokenverifier"
-
-#TIER5
-#addRepro "cora-metacreator"
-#addRepro "cora-systemone"
-
-#TIER6
-#addRepro "cora-jsclient"
-#addRepro "cora-fitnesse"
-
-
-#addRepro "cora-docker-gatekeeper"
-#addRepro "cora-docker-therest"
-#addRepro "cora-docker-fitnesse"
+# ################# calls start here #######################################
+chooseRepo
+importProjectListing
+preventGitAskingForUsernameAndPasswordIfRepoIsMissing
+addAllRepositories
+setBasePathToPointToBasicStorageWorkspaceDirectoryInTomcatContextXml
