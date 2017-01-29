feabf5f8a4aef9d565403b84b935d89bed0bf9d4
olov <olov@472dc6bc65b0>
2017-01-29 16:24:22 +0000

    moving setupProjects

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,7 +4,7 @@ firstRun(){
 	git clone https://github.com/olovm/cora-eclipse.git ~/workspace/cora-eclipse
 	
 	chmod +x ~/workspace/cora-eclipse/docker/setupProjects.sh
-	~/workspace/cora-eclipse/docker/setupProjects.sh "~/workspace"
+	~/workspace/cora-eclipse/development/setupProjects.sh "~/workspace"
 	
 	cd ~/workspace/cora-jsclient/
 	npm install karma karma-chrome-launcher karma-firefox-launcher karma-qunit karma-coverage karma-html-reporter qunitjs --save-dev
