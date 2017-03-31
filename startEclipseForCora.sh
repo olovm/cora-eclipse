77744563f4af95cad3d54b17c19fe62fa2872cb5
olov <olov@e2d5ad4c938c>
2017-03-31 08:56:34 +0000

    updates for new version Neon .3 etc

--- startEclipseForCora.sh
+++ startEclipseForCora.sh
@@ -5,7 +5,7 @@ BASEDIR=$(dirname $BASH_SOURCE)
 
 
 if [ ! $USER ]; then
-  	echo "You must specify the userName used when building eclipseforcora1"
+  	echo "You must specify the userName used when building eclipseforcora2"
 else
 	docker run --rm -ti --privileged -e DISPLAY=$DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
@@ -15,5 +15,5 @@ else
 	-v INSTALLDIR/m2:/home/$USER/.m2 \
 	-v PARENTDIR/eclipseP2:/home/$USER/.p2 \
 	-v PARENTDIR/.gitconfig:/home/$USER/.gitconfig \
-	--env user=$USER -p 8080:8080 -p 9876:9876 -p 8090:8090 --name eclipseforcora1 eclipseforcora1 $2
+	--env user=$USER -p 8080:8080 -p 9876:9876 -p 8090:8090 --name eclipseforcora2 eclipseforcora2 $2
 fi
