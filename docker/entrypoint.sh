da43daeb6b329e830e30733928ead16198c46c7e
olov <olov@eb6729cf3e96>
2017-03-31 09:45:03 +0000

    updates for new version Neon .3 etc

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,14 +8,19 @@ firstRun(){
 	
 	cd ~/workspace/cora-jsclient/
 	npm install karma karma-chrome-launcher karma-firefox-launcher karma-qunit karma-coverage karma-html-reporter qunitjs --save-dev
-	
+
+	runInstaller	
+}
+
+runInstaller(){
 	#SWT_GTK3=0  ~/eclipse-installer/eclipse-inst
 	~/eclipse-installer/eclipse-inst
 }
 
-
 if [ ! -d ~/workspace/cora-jsclient ]; then
   	firstRun
+if [ ! -d ~/eclipse/eclipseforcora ]; then
+	runInstaller
 else
 	SWT_GTK3=0 ~/eclipse/eclipseforcora/eclipse
 fi
