d2f46a827739f3a410063729a79a99bac4471fc0
olov <olov@e2d5ad4c938c>
2017-03-31 08:58:53 +0000

    updates for new version Neon .3 etc

--- buildEclipseForCora.sh
+++ buildEclipseForCora.sh
@@ -3,7 +3,7 @@
 USER=$1
 
 if [ ! $USER ]; then
-  	echo you must specify the userName to be used when building eclipseforcora1
+  	echo you must specify the userName to be used when building eclipseforcora2
 else
-	docker build --build-arg user=$USER -t eclipseforcora1 docker/
+	docker build --build-arg user=$USER -t eclipseforcora2 docker/
 fi
