5ef493747964c3a414126dd85b1c8b727a648464
olov <olov@7821d87ed9cc>
2017-09-16 13:30:54 +0000

    will try to add postgres as a standalone second container instead

--- buildEclipseForCora.sh
+++ buildEclipseForCora.sh
@@ -7,5 +7,6 @@ if [ ! $USER ]; then
 else
 	#for possibly newer version of from: X
 	#docker build --pull --build-arg user=$USER -t eclipseforcoraoxygen1 cora-eclipse/docker/
-	docker build --build-arg user=$USER -t eclipseforcoraoxygen1 cora-eclipse/docker/
+	#docker build --build-arg user=$USER -t eclipseforcoraoxygen1 cora-eclipse/docker/
+	docker-compose build -e user
 fi
--- startEclipseForCora.sh
+++ startEclipseForCora.sh
@@ -5,9 +5,10 @@ BASEDIR=$(dirname $BASH_SOURCE)
 
 
 if [ ! $USER ]; then
-  	echo "You must specify the userName used when building eclipseforcoraoxygen1"
+  	echo "You must specify the userName used when starting eclipseforcoraoxygen1"
 else
-	docker run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
+	#docker run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
+	docker-compose run -e DISPLAY \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v INSTALLDIR/workspace:/home/$USER/workspace \
 	-v INSTALLDIR/eclipse:/home/$USER/eclipse \
@@ -16,5 +17,5 @@ else
 	-v PARENTDIR/eclipseP2:/home/$USER/.p2 \
 	-v PARENTDIR/.gitconfig:/home/$USER/.gitconfig \
 	-v PARENTDIR/solr:/opt/solr-6.6.0/server/solr \
-	--env user=$USER -p 8080:8080 -p 9876:9876 -p 8090:8090 -p 8983:8983 --name eclipseforcoraoxogen1 eclipseforcoraoxygen1 $2
+	--env user=$USER -p 8080:8080 -p 9876:9876 -p 8090:8090 -p 8983:8983 eclipseoxygen0 $2
 fi
