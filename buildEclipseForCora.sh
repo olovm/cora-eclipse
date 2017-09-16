eacf86bed9b66d4260ce514d05d256fbe5d0f357
olov <olov@7821d87ed9cc>
2017-09-16 13:38:08 +0000

    will try to add postgres as a standalone second container instead

--- buildEclipseForCora.sh
+++ buildEclipseForCora.sh
@@ -8,5 +8,6 @@ else
 	#for possibly newer version of from: X
 	#docker build --pull --build-arg user=$USER -t eclipseforcoraoxygen1 cora-eclipse/docker/
 	#docker build --build-arg user=$USER -t eclipseforcoraoxygen1 cora-eclipse/docker/
-	docker-compose build --build-arg user=$USER cora-eclipse/docker/
+	cd cora-eclipse/docker/
+	docker-compose build --build-arg user=$USER 
 fi
