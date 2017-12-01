a52e6ef3f145c605b914592204278e39f2c02c3f
olov <olov@562389a23659>
2017-12-01 15:53:57 +0000

    adding idplogin as project

--- development/projectListing.sh
+++ development/projectListing.sh
@@ -6,14 +6,15 @@ TIER0="cora-parent"
 TIER1="cora-json cora-httphandler cora-userpicker cora-apptokenstorage cora-searchstorage"
 TIER2="cora-beefeater cora-bookkeeper cora-gatekeeper "
 TIER3="cora-spider cora-gatekeepertokenprovider"
-TIER4="cora-basicstorage cora-sqlstorage cora-gatekeeperclient cora-therest cora-apptokenverifier cora-solrsearch"
+TIER4="cora-basicstorage cora-sqlstorage cora-gatekeeperclient cora-therest cora-apptokenverifier "
+TIER4+="cora-idplogin cora-solrsearch"
 TIER5="cora-metacreator cora-systemone "
 #TIER6="cora-jsclient cora-fitnesse "
 TIER6_JS="cora-jsclient  "
 TIER6="cora-fitnesse "
 
 
-TIER_DOCKER="cora-docker-gatekeeper cora-docker-apptokenverifier "
+TIER_DOCKER="cora-docker-gatekeeper cora-docker-apptokenverifier cora-docker-idplogin "
 TIER_DOCKER+="cora-docker-solr cora-docker-therest cora-docker-fitnesse "
 
 ALL_JAVA=$TIER0" "$TIER1" "$TIER2" "$TIER3" "$TIER4" "$TIER5" "$TIER6
