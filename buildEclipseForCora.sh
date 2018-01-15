841e4b7daa51211b58a74d3dec73544f555c570f
olov <olov@4123fdd4e651>
2018-01-15 13:25:35 +0000

    trying build script

--- buildEclipseForCora.sh
+++ buildEclipseForCora.sh
@@ -7,7 +7,8 @@ if [ ! $USER ]; then
 else
 	#for possibly newer version of from: X
 	#docker build --pull --build-arg user=$USER -t eclipseforcoraoxygen1 cora-eclipse/docker/
-	docker build --build-arg user=$USER -t eclipseforcoraoxygen2 cora-eclipse/docker/
+	#docker build --build-arg user=$USER -t eclipseforcoraoxygen2 cora-eclipse/docker/
 	cd cora-eclipse/docker/
-	docker-compose build --build-arg user=$USER eclipseoxygen2
+	#docker-compose build --build-arg user=$USER eclipseforcoraoxygen2
+	docker-compose build --build-arg user=$USER
 fi
