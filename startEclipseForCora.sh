f87873fbf78696465b685aa1fab6af34aca06b65
olov <olov@9923e85c0b52>
2018-01-17 07:28:49 +0000

    start work on sibbling containers, adding docker socket

--- startEclipseForCora.sh
+++ startEclipseForCora.sh
@@ -9,6 +9,7 @@ else
 	#docker run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
 cd eclipseForCora
 docker-compose run -e DISPLAY=$DISPLAY\
+ -v /var/run/docker.sock:/var/run/docker.sock\
  -v /tmp/.X11-unix:/tmp/.X11-unix\
  -v INSTALLDIR/workspace:/home/$USER/workspace\
  -v INSTALLDIR/eclipse:/home/$USER/eclipse\
