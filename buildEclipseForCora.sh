1f030dbac8694231b7923756e3ff48a4a01824c1
olov <olov@c7557bbda73c>
2018-02-09 13:11:11 +0000

    test to run docker from docker

--- buildEclipseForCora.sh
+++ buildEclipseForCora.sh
@@ -10,7 +10,7 @@ elif [ ! $DOCKERGROUPID ]; then
 else
 	#for possibly newer version of from: X
 	#docker build --pull --build-arg user=$USER dockergroupid=$DOCKERGROUPID -t eclipseforcoraoxygen1 cora-eclipse/docker/
-	docker build --build-arg user=$USER --build-arg dockergroupid=$DOCKERGROUPID -t eclipseforcoraoxygen1 cora-eclipse/docker/
+	docker build --build-arg user=$USER --build-arg dockergroupid=$DOCKERGROUPID -t eclipseforcoraoxygen21 cora-eclipse/docker/
 	#docker build --build-arg user=$USER -t eclipseforcoraoxygen2 cora-eclipse/docker/
 	#cd cora-eclipse/docker/
 	#docker-compose build --build-arg user=$USER eclipseforcoraoxygen2
