08187d18afed36ce9d4e0e73dd9cac5598d61799
olov <olov@62cb1aa530a0>
2018-02-16 14:05:37 +0000

    adding cora-docker-fedora to dev build

--- development/projectListing.sh
+++ development/projectListing.sh
@@ -14,8 +14,8 @@ TIER6="cora-fitnesseintegration cora-fitnesse "
 TIER6_JS="cora-jsclient  "
 
 CORA_DOCKER="cora-docker-gatekeeper cora-docker-apptokenverifier cora-docker-idplogin "
-CORA_DEV_DOCKER="cora-docker-solr "
-CORA_DOCKER+="cora-docker-therest cora-docker-fitnesse cora-docker-fedora "
+CORA_DEV_DOCKER="cora-docker-solr cora-docker-fedora "
+CORA_DOCKER+="cora-docker-therest cora-docker-fitnesse "
 CORA_DOCKER+="cora-docker-jsclient cora-docker-postgresql "
 
 
