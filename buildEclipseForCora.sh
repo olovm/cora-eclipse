8421c3da34156598ebec9133831f70b576b5a32d
olov <olov@5305d2de1c09>
2018-09-05 08:05:24 +0000

    adding userid to buildEclipseForDocker as it might not always be 1000

--- buildEclipseForCora.sh
+++ buildEclipseForCora.sh
@@ -6,14 +6,14 @@ DOCKERGROUPID=$3
 
 if [ ! $USER ]; then
   	echo you must specify the userName to be used when building eclipseforcoraphoton
-elif [ ! $DOCKERGROUPID ]; then
+elif [ ! $USERID ]; then
 	echo you must specify the userid to be used when building eclipseforcoraphoton, use: id -u youruserid 
 elif [ ! $DOCKERGROUPID ]; then
 	echo you must specify the dockergroupid to be used when building eclipseforcoraphoton, use: getent group docker 
 else
 	#for possibly newer version of from: X
 	#docker build --pull --no-cache --build-arg user=$USER --build-arg dockergroupid=$DOCKERGROUPID -t eclipseforcoraoxygen2 cora-eclipse/docker/
-	docker build --build-arg user=$USER --build-arg user=$USERID --build-arg dockergroupid=$DOCKERGROUPID -t eclipseforcoraphoton1 cora-eclipse/docker/
+	docker build --build-arg user=$USER --build-arg userid=$USERID --build-arg dockergroupid=$DOCKERGROUPID -t eclipseforcoraphoton1 cora-eclipse/docker/
 	#docker build --build-arg user=$USER -t eclipseforcoraoxygen2 cora-eclipse/docker/
 	#cd cora-eclipse/docker/
 	#docker-compose build --build-arg user=$USER eclipseforcoraoxygen2
