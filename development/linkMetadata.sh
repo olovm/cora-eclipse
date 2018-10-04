48cd9be99f0310b2451d62f64bbee4e7470b3b51
olov <olov@076d9bc7933a>
2018-10-04 06:12:06 +0000

    code cleanup in linking sripts

--- development/linkMetadata.sh
+++ development/linkMetadata.sh
@@ -13,21 +13,30 @@ createMetadataDirectories(){
 	mkdir $workspaceDir/metadata/alvin -p
 	mkdir $workspaceDir/metadata/diva -p
 }
+
 linkMetadata(){
+	linkMetadataToSystemOne;
+	linkMetadataToAlvin;	
+	linkMetadataToDiVA;
+}
+
+linkMetadataToSystemOne(){
 	ln -s $workspaceDir/cora-metadata/files/cora $workspaceDir/metadata/systemone/cora
 	ln -s $workspaceDir/cora-metadata/files/jsClient $workspaceDir/metadata/systemone/jsClient
 	ln -s $workspaceDir/cora-metadata/files/testSystem $workspaceDir/metadata/systemone/testSystem
 	ln -s $workspaceDir/systemone-metadata/files/systemOne $workspaceDir/metadata/systemone/systemOne
-	
+}
+
+linkMetadataToAlvin(){
 	ln -s $workspaceDir/cora-metadata/files/cora $workspaceDir/metadata/alvin/cora
 	ln -s $workspaceDir/cora-metadata/files/jsClient $workspaceDir/metadata/alvin/jsClient
 	ln -s $workspaceDir/alvin-metadata/files/alvin $workspaceDir/metadata/alvin/alvin
 	ln -s $workspaceDir/alvin-metadata/files/bibsys $workspaceDir/metadata/alvin/bibsys
 #temporaraly link in  systemone
 	ln -s $workspaceDir/systemone-metadata/files/systemOne $workspaceDir/metadata/alvin/systemOne
-	#ln -s /home/olov/workspace/systemone-metadata/files/systemOne /home/olov/workspace/metadata/alvin/systemOne
-	#ln -s /home/olov/workspace/systemone-metadata/files/systemOne /home/olov/workspace/metadata/diva/systemOne
-	
+}
+
+linkMetadataToDiVA(){
 	ln -s $workspaceDir/cora-metadata/files/cora $workspaceDir/metadata/diva/cora
 	ln -s $workspaceDir/cora-metadata/files/jsClient $workspaceDir/metadata/diva/jsClient
 	ln -s $workspaceDir/diva-metadata/files/diva $workspaceDir/metadata/diva/diva
@@ -35,6 +44,7 @@ linkMetadata(){
 #temporaraly link in systemOne
 	ln -s $workspaceDir/systemone-metadata/files/systemOne $workspaceDir/metadata/diva/systemOne
 }
+
 # ################# calls start here #######################################
 if [ ! $workspaceDir ]; then
   	echo "You must specify the workspace directory.."
--- development/postInstaller.sh
+++ development/postInstaller.sh
@@ -9,21 +9,12 @@ USER=$(id -u -n)
 
 start(){
 	changeUserInContexts
-#	linkJsClientToTomcats
 }
 
 changeUserInContexts(){
 	find /home/$USER/workspace/cora-eclipse/oomph/Servers/ -type f -exec sed -i -e 's/olov/'"$USER"'/g' {} \;
 }
 
-linkJsClientToTomcats(){
-	mkdir -p /home/$USER/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/webapps
-	mkdir -p /home/$USER/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp1/webapps
-	mkdir -p /home/$USER/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/webapps
-	ln -s /home/$USER/workspace/cora-jsclient/src/main/webapp/ /home/$USER/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/webapps/jsclient
-	ln -s /home/$USER/workspace/cora-jsclient/src/main/webapp/ /home/$USER/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp1/webapps/jsclient
-	ln -s /home/$USER/workspace/cora-jsclient/src/main/webapp/ /home/$USER/workspace/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/webapps/jsclient
-}
 
 # ################# calls start here #######################################
 if [ ! $workspaceDir ]; then
