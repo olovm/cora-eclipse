6dfa8d898247df21d0242fe96075596cc2962aa3
olov <olov@405d41dce47d>
2018-10-09 14:54:23 +0000

    adding support to build with maven and temporary pom file
    This is a start... :)

---
+++ development/mvnPomCleanInstallAllButDocker.sh
@@ -0,0 +1,51 @@
+#! /usr/bin/env bash
+userChoosenGroup=$1;
+
+start(){
+	importProjectListing;
+	PROJECTS_TO_RUN="${!userChoosenGroup}";
+	checkChoosenProjectsExists;
+	createTempPomFile;
+	cleanInstallAllUsingTempPomFile;	
+}
+
+importProjectListing(){
+	.  ~/workspace/cora-eclipse/development/projectListing.sh
+}
+
+checkChoosenProjectsExists(){
+	if [  ${#PROJECTS_TO_RUN} -lt 1 ]; then 
+		echo "Error, choosen group ($userChoosenGroup) does not exist in projectListings.sh."
+		exit 1;
+	fi
+}
+
+createTempPomFile(){
+	copyTempPomFileToWorkspace;
+	addAllProjectsToTempPomFile;
+	writeEndToTempPomFile;
+}
+
+copyTempPomFileToWorkspace(){
+	touch ~/workspace/tempPom.xml
+	cp ~/workspace/cora-eclipse/development/modulePomTemplate.xml ~/workspace/tempPom.xml
+}
+
+addAllProjectsToTempPomFile(){
+	for PROJECT in $PROJECTS_TO_RUN; do
+		echo "adding project $PROJECT"
+		echo "<module>$PROJECT</module>" >>  ~/workspace/tempPom.xml
+	done
+}
+
+writeEndToTempPomFile(){
+		echo "</modules>" >>  ~/workspace/tempPom.xml
+		echo "</project>" >>  ~/workspace/tempPom.xml
+}
+
+cleanInstallAllUsingTempPomFile(){
+	mvn clean install -T 1.5C -f ~/workspace/tempPom.xml
+}	
+
+# ################# calls start here #######################################
+start;
