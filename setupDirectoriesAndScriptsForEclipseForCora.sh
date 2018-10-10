955b1b8753d0dcbc0f4998987083a292595cabf8
olov <olov@f69b5ca6f64b>
2018-10-10 15:45:51 +0000

    Experimenting with other startup for setup run
    Experimenting with special setup starting to enable building new while
    current is running

--- setupDirectoriesAndScriptsForEclipseForCora.sh
+++ setupDirectoriesAndScriptsForEclipseForCora.sh
@@ -29,6 +29,10 @@ changeAndCopyScripts(){
 	cp $BASEDIR/startEclipseForCora.sh $PARENTDIR/eclipseForCora/
 	sed -i "s|INSTALLDIR|$INSTALLDIR|g" $PARENTDIR/eclipseForCora/startEclipseForCora.sh
 	sed -i "s|PARENTDIR|$PARENTDIR|g" $PARENTDIR/eclipseForCora/startEclipseForCora.sh
+
+	cp $BASEDIR/startEclipseForCoraTempSetup.sh $PARENTDIR/eclipseForCora/
+	sed -i "s|INSTALLDIR|$INSTALLDIR|g" $PARENTDIR/eclipseForCora/startEclipseForCoraTempSetup.sh
+	sed -i "s|PARENTDIR|$PARENTDIR|g" $PARENTDIR/eclipseForCora/startEclipseForCoraTempSetup.sh
 	
 	#cp $BASEDIR/buildEclipseForCora.sh $PARENTDIR/eclipseForCora/
 	#sed -i "s|INSTALLDIR|$INSTALLDIR|g" $PARENTDIR/eclipseForCora/buildEclipseForCora.sh
