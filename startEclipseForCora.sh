12c1b3de25cea452c3222c8301b80befa57bcd4c
olov <olov@4fc5e2a2c915>
2018-10-17 11:55:52 +0000

    adding FitNesse ports to startup to acces from outside

--- startEclipseForCora.sh
+++ startEclipseForCora.sh
@@ -23,9 +23,13 @@ docker run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPL
  -v PARENTDIR/eclipseP2:/home/$USER/.p2\
  -v PARENTDIR/.gitconfig:/home/$USER/.gitconfig\
  -e user=$USER\
- -p 8080:8080 -p 9876:9876 -p 8090:8090\
- -p 8081:8081\
+ -p 9876:9876 \
+ -p 8080:8080 \
+ -p 8081:8081 \
  -p 8082:8082\
+ -p 8090:8090 \
+ -p 8091:8091 \
+ -p 8092:8092 \
  --network=eclipseForCoraNet\
  --name eclipse201809forcora4\
  eclipse201809forcora4 $2
