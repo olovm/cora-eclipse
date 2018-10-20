1843c7b4b7c6faabb883e37de71ab795fe278406
olov <olov@958fe096bc0b>
2018-10-20 12:47:17 +0000

    removing ports exposed directly from their containers

--- startEclipseForCora.sh
+++ startEclipseForCora.sh
@@ -23,9 +23,6 @@ docker run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPL
  -v PARENTDIR/eclipseP2:/home/$USER/.p2\
  -v PARENTDIR/.gitconfig:/home/$USER/.gitconfig\
  -e user=$USER\
- -p 5432:5432 \
- -p 5432:5433 \
- -p 5432:5434 \
  -p 9876:9876 \
  -p 8080:8080 \
  -p 8081:8081 \
@@ -33,9 +30,6 @@ docker run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPL
  -p 8090:8090 \
  -p 8091:8091 \
  -p 8092:8092 \
- -p 8983:8983 \
- -p 8984:8984 \
- -p 8985:8985 \
  --network=eclipseForCoraNet\
  --name eclipse201809forcora4\
  eclipse201809forcora4 $2
@@ -45,10 +39,10 @@ docker run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPL
  cd ../
 fi
 
-#5432 postgresql
+#5432 postgresql (exposed directly from that container)
 #9876 karma
 #8080 tomcat
 #8090 fitnesse
-#8983 solr
+#8983 solr (exposed directly from that container)
 
 #  -p 8080:8080 -p 9876:9876 -p 8090:8090 -p 8983:8983 -p 5432:5432\
