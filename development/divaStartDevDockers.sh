22b7e03b6547a84a25fbf14961ade989ba51fc24
olovm <olov.mckie@ub.uu.se>
2018-11-29 12:18:54 +0000

    adding diva-cora-docker-postgresql

--- development/divaStartDevDockers.sh
+++ development/divaStartDevDockers.sh
@@ -3,7 +3,7 @@
 echo "starting solr"
 docker run -d --name diva-cora-solr \
 --network=eclipseForCoraNet \
--p 8983:8985 \
+-p 8985:8983 \
 cora-solr:0.3-SNAPSHOT \
 solr-precreate coracore /opt/solr/server/solr/configsets/coradefaultcore
 docker start diva-cora-solr
@@ -11,7 +11,7 @@ docker start diva-cora-solr
 echo "starting postgresql for fedora"
 docker run -d --name diva-postgres-fcrepo --rm \
 --net-alias=postgres-fcrepo \
--p 5432:5434 \
+-p 5434:5432 \
 --network=eclipseForDivaNet \
 -e POSTGRES_DB=fedora38 \
 -e POSTGRES_USER=fedoraAdmin \
@@ -28,3 +28,13 @@ diva-cora-docker-fedora:3.8.1 /home/fedora/checkAndStart.sh
 
 echo "connecting fedora docker to eclipseForCoraNet to access from tomcat and main application"
 docker network connect eclipseForCoraNet diva-docker-fedora
+
+echo "starting postgresql with diva data"
+docker run -d --name diva-cora-docker-postgresql --restart always  \
+--net-alias=postgres-diva \
+-p 5435:5432 \
+--network=eclipseForDivaNet \
+-e POSTGRES_DB=diva \
+-e POSTGRES_USER=diva \
+-e POSTGRES_PASSWORD=diva \
+diva-cora-docker-postgresql 
