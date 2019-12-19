3d00fc675801cccc88955d6ecb04366e4a04f7e5
olovm <olov.mckie@ub.uu.se>
2019-12-19 13:24:52 +0000

    podman or docker

--- startEclipseForCoraTempSetup.sh
+++ startEclipseForCoraTempSetup.sh
@@ -14,7 +14,7 @@ echo
 echo "Testing for container runtimes...."
 CONTAINERRUNTIME=podman;
 DOCKER_EXISTS=$(command -v docker)
-
+echo "Docker size: "${#DOCKER_EXISTS}
 if [ ${#DOCKER_EXISTS} -gt 0 ]; then
 	CONCONTAINERRUNTIME=docker;
 fi
