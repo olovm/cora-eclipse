b395cd4037cde65772f46c27f06cfcb5d1f9e01e
olov <olov@c37c63a112e4>
2020-03-27 11:53:18 +0000

    new name  eclipse202003forcora2

--- setupDirectoriesAndScriptsForEclipseForCora.sh
+++ setupDirectoriesAndScriptsForEclipseForCora.sh
@@ -6,7 +6,7 @@ echo "Running setupDirectoriesAndScriptsForEclipseForCora..."
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname $SCRIPT)
 PARENTDIR="$(dirname "$BASEDIR")"
-INSTALLDIR=$PARENTDIR/eclipse202003forcora1
+INSTALLDIR=$PARENTDIR/eclipse202003forcora2
 TOPDIR="$(dirname "$PARENTDIR")"
 
 echo 
