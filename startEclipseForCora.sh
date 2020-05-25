9243663f076ac41b1feb841b689aab3b271fd5e1
olov <olov@54e376ad45ca>
2020-05-25 08:22:49 +0000

    changing to fedora 32 and new name eclipse202003forcora3

--- startEclipseForCora.sh
+++ startEclipseForCora.sh
@@ -18,10 +18,10 @@ fi
 echo "Container runtime will be "${CONTAINERRUNTIME}
 
 if [ ! $USER ]; then
-  	echo "You must specify the userName used when starting eclipse202003forcora2"
+  	echo "You must specify the userName used when starting eclipse202003forcora3"
 else
 	#${CONTAINERRUNTIME} run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
-cd eclipse202003forcora2
+cd eclipse202003forcora3
 #docker-compose run -e DISPLAY=$DISPLAY\
 #${CONTAINERRUNTIME} run --rm -ti --privileged --net=host --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
 ${CONTAINERRUNTIME} run --rm -ti --privileged  --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
@@ -49,8 +49,8 @@ ${CONTAINERRUNTIME} run --rm -ti --privileged  --ipc=host --env="QT_X11_NO_MITSH
  -p 8091:8091 \
  -p 8092:8092 \
  --network=eclipseForCoraNet\
- --name eclipse202003forcora2\
- eclipse202003forcora2 $2
+ --name eclipse202003forcora3\
+ eclipse202003forcora3 $2
  cd ../
 fi
 
