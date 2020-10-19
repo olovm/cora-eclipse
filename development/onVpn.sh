34d012691b84117fa3f239e352b24466754faf00
perebartrolisimo <54933118+perebartrolisimo@users.noreply.github.com>
2020-10-19 18:33:22 +0200

    Update onVpn.sh
    Wrong docker name

--- development/onVpn.sh
+++ development/onVpn.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-docker network disconnect eclipseForCoraNet eclipse202009forcora1
+docker network disconnect eclipseForCoraNet eclipse202009forcora1NoPorts
 
 docker network create tempvpn
 
-docker network connect tempvpn eclipse202009forcora1
+docker network connect tempvpn eclipse202009forcora1NoPorts
