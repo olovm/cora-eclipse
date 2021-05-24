a3609ee6ec6354f2ba7d26bcb68afb7a529de1e9
olovm <olov.mckie@ub.uu.se>
2021-05-24 13:35:48 +0000

    changing name to eclipse202103forcora3

--- setupDirectoriesAndScriptsForEclipseForCora.sh
+++ setupDirectoriesAndScriptsForEclipseForCora.sh
@@ -6,7 +6,7 @@ echo "Running setupDirectoriesAndScriptsForEclipseForCora..."
 SCRIPT=$(readlink -f "$0")
 BASEDIR=$(dirname $SCRIPT)
 PARENTDIR="$(dirname "$BASEDIR")"
-INSTALLDIR=$PARENTDIR/eclipse202103forcora2
+INSTALLDIR=$PARENTDIR/eclipse202103forcora3
 TOPDIR="$(dirname "$PARENTDIR")"
 
 echo 
--- startEclipseForCora.sh
+++ startEclipseForCora.sh
@@ -19,10 +19,10 @@ fi
 echo "Container runtime will be "${CONTAINERRUNTIME}
 
 if [ ! $USER ]; then
-  	echo "You must specify the userName used when starting eclipse202103forcora2"
+  	echo "You must specify the userName used when starting eclipse202103forcora3"
 else
 	#${CONTAINERRUNTIME} run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
-cd eclipse202103forcora2
+cd eclipse202103forcora3
 #docker-compose run -e DISPLAY=$DISPLAY\
 #${CONTAINERRUNTIME} run --rm -ti --privileged --net=host --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
 ${CONTAINERRUNTIME} run --rm -ti --privileged  --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
@@ -54,8 +54,8 @@ ${CONTAINERRUNTIME} run --rm -ti --privileged  --ipc=host --env="QT_X11_NO_MITSH
  -p 38091:8091\
  -p 38092:8092\
  --network=eclipseForCoraNet\
- --name eclipse202103forcora2\
- eclipse202103forcora2 $2
+ --name eclipse202103forcora3\
+ eclipse202103forcora3 $2
  cd ../
 fi
 
