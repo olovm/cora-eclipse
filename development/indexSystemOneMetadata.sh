a9f8be4076a4de1e7735f1b6ae94e72bf1b71e18
olovm <olov.mckie@ub.uu.se>
2022-05-17 00:07:14 +0000

    working on indexing of metadata

---
+++ development/indexSystemOneMetadata.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+
+#INDEX_URL='https://cora.epc.ub.uu.se/systemone/rest/record/index'
+LOGIN_URL='http://localhost:8180/apptokenverifier/rest/apptoken/141414'
+INDEX_URL='http://localhost:8080/systemone/rest/record/index'
+APP_TOKEN='63e6bd34-02a1-4c82-8001-158c104cae0e'
+start(){
+	login;
+	#indexMetadata 'metadata';
+	indexMetadata 'text';
+	#indexMetadata 'collectTerm';
+	#indexMetadata 'presentation';
+	#indexMetadata 'guiElement';
+	#indexMetadata 'system';
+	logoutFromCora;
+}
+login(){
+	#AUTH_TOKEN=$(curl -s -X POST -k -i ${LOGIN_URL} --data ${APP_TOKEN} | grep -o -P '(?<={"name":"id","value":").*?(?="})')
+	local loginAnswer=$(curl -s -X POST -k -i ${LOGIN_URL} --data ${APP_TOKEN});
+	echo 'LoginAnswer: '${loginAnswer} 
+	AUTH_TOKEN=$(echo ${loginAnswer} | grep -o -P '(?<={"name":"id","value":").*?(?="})')
+	echo 'Logged in, got authToken: '${AUTH_TOKEN} 
+}
+indexMetadata(){
+	echo ""
+	local recordType=$1
+	echo 'Indexing recordType: '${recordType}
+	curl -s -X POST -k -H 'authToken: '${AUTH_TOKEN} -i ${INDEX_URL}'/'${recordType} | grep -o -P '(?<={"name":"id","value":").*?(?="})'
+}
+logoutFromCora(){
+	echo ""
+	curl -s -X DELETE -k  'http://localhost:8180/apptokenverifier/rest/apptoken/141414' --data ${AUTH_TOKEN} 
+	echo 'Logged out' 
+}
+
+# ################# calls start here #######################################
+start
