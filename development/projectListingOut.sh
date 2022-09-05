fb5fdbdd8687c7c5339a50eb56083ae4f0531ca8
olovm <olov.mckie@ub.uu.se>
2022-09-05 14:13:54 +0000

    fixing some stuff in project listing out

--- development/projectListingOut.sh
+++ development/projectListingOut.sh
@@ -11,12 +11,16 @@ printProjects() {
 
 loopProjectsNotInUse() {
     N=10;
+    local outString;
 	for PROJECT in $ALL; do
 		((i=i%N)); ((i++==0)) && wait
-		echo $PROJECT &
+		#echo $PROJECT, &
+		outString=$outString$PROJECT,
 	done
         wait      # catch the processes that we did not wait for earlier
+    echo $outString;
 }
 
 importProjectListing
-printProjects
+#printProjects
+loopProjectsNotInUse
