1b79840d8032a61f4c83d1a5798736cfc6b32963
olovm <olov.mckie@ub.uu.se>
2023-02-20 14:33:28 +0000

    Updated tomcat version to 10.1.5

--- development/offVpn.sh
+++ development/offVpn.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-docker network disconnect tempvpn eclipse202212forcora2
+docker network disconnect tempvpn eclipse202212forcora3
 
 docker network rm tempvpn
 
-docker network connect eclipseForCoraNet eclipse202212forcora2
+docker network connect eclipseForCoraNet eclipse202212forcora3
--- startEclipseForCoraWayland.sh
+++ startEclipseForCoraWayland.sh
@@ -19,10 +19,10 @@ fi
 echo "Container runtime will be "${CONTAINERRUNTIME}
 
 if [ ! $USER ]; then
-  	echo "You must specify the userName used when starting eclipse202212forcora2"
+  	echo "You must specify the userName used when starting eclipse202212forcora3"
 else
 	#${CONTAINERRUNTIME} run --rm -ti --privileged --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
-cd eclipse202212forcora2
+cd eclipse202212forcora3
 #docker-compose run -e DISPLAY=$DISPLAY\
 #${CONTAINERRUNTIME} run --rm -ti --privileged --net=host --ipc=host --env="QT_X11_NO_MITSHM=1"  -e DISPLAY=$DISPLAY \
 ${CONTAINERRUNTIME} run --rm -ti --privileged  --ipc=host \
@@ -33,10 +33,10 @@ ${CONTAINERRUNTIME} run --rm -ti --privileged  --ipc=host \
  -v $XDG_RUNTIME_DIR/$WAYLAND_DISPLAY:/tmp/$WAYLAND_DISPLAY \
  -v /var/run/docker.sock:/var/run/docker.sock\
  -v /tmp/.X11-unix:/tmp/.X11-unix\
- -v /mnt/depot/cora/eclipse202212forcora2/workspace:/home/$USER/workspace\
- -v /mnt/depot/cora/eclipse202212forcora2/eclipse:/home/$USER/eclipse\
- -v /mnt/depot/cora/eclipse202212forcora2/.eclipse:/home/$USER/.eclipse\
- -v /mnt/depot/cora/eclipse202212forcora2/.saros:/home/$USER/.saros\
+ -v /mnt/depot/cora/eclipse202212forcora3/workspace:/home/$USER/workspace\
+ -v /mnt/depot/cora/eclipse202212forcora3/eclipse:/home/$USER/eclipse\
+ -v /mnt/depot/cora/eclipse202212forcora3/.eclipse:/home/$USER/.eclipse\
+ -v /mnt/depot/cora/eclipse202212forcora3/.saros:/home/$USER/.saros\
  -v /mnt/depot/cora/m2:/home/$USER/.m2\
  -v /mnt/depot/cora/eclipseP2:/home/$USER/.p2\
  -v /mnt/depot/cora/.gitconfig:/home/$USER/.gitconfig\
@@ -59,8 +59,8 @@ ${CONTAINERRUNTIME} run --rm -ti --privileged  --ipc=host \
  -p 38091:8091\
  -p 38092:8092\
  --network=eclipseForCoraNet\
- --name eclipse202212forcora2\
- eclipse202212forcora2 $2
+ --name eclipse202212forcora3\
+ eclipse202212forcora3 $2
  cd ../
 fi
 
