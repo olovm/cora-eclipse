4b510b57669e1b54b89ccde7d92dd81d39991233
olovm <olov.mckie@ub.uu.se>
2024-05-31 16:47:18 +0000

    reindex

--- development/indexAlvinMetadata.sh
+++ development/indexAlvinMetadata.sh
@@ -1,10 +1,14 @@
 #! /bin/bash
 
-#INDEX_URL='https://cora.epc.ub.uu.se/systemone/rest/record/index'
+INDEX_URL='https://cora.epc.ub.uu.se/systemone/rest/record/index'
 LOGIN_URL='http://localhost:8181/login/rest/apptoken/141414'
 LOGOUT_URL='http://localhost:8181/login/rest/authToken/141414'
 INDEX_URL='http://localhost:8081/alvin/rest/record/index'
 APP_TOKEN='63e6bd34-02a1-4c82-8001-158c104cae0e'
+#LOGIN_URL='https://cora.alvin-portal.org/login/rest/apptoken/141414'
+#LOGOUT_URL='https://cora.alvin-portal.org/login/rest/authToken/141414'
+#INDEX_URL='https://cora.alvin-portal.org/rest/record/index'
+#APP_TOKEN='63e6bd34-02a1-4c82-8001-158c104cae0e'
 start(){
 	login;
 	indexMetadata 'recordType';
@@ -15,6 +19,8 @@ start(){
 	indexMetadata 'presentation';
 	indexMetadata 'guiElement';
 	indexMetadata 'system';
+#	indexMetadata 'alvin-place';
+#	indexMetadata 'alvin-person';
 	logoutFromCora;
 }
 login(){
@@ -28,8 +34,11 @@ indexMetadata(){
 	echo ""
 	local recordType=$1
 	echo 'Indexing recordType: '${recordType}
-	curl -s -X POST -k -H 'authToken: '${AUTH_TOKEN} -i ${INDEX_URL}'/'${recordType} | grep -o -P '(?<={"name":"id","value":").*?(?="})'
+	local indexAnswer=$(curl -s -X POST -k -H 'authToken: '${AUTH_TOKEN} -i ${INDEX_URL}'/'${recordType} )
+	echo 'IndexAnswer: '${indexAnswer} 
+#curl -s -X POST -k -H 'authToken: '9341f0bb-584a-492a-ae91-16169fa36bd0 -i https://cora.alvin-portal.org/alvin/rest/record/index/alvin-place 
 }
+
 logoutFromCora(){
 	echo ""
 	curl -s -X DELETE -H "Content-Type: text/plain" -k  $LOGOUT_URL --data ${AUTH_TOKEN} 
