48c45bb5887e34a52e9faa737bfc36f25d586e89
olovm <olov.mckie@ub.uu.se>
2025-10-21 20:09:51 +0000

    fixes delete batch job

--- development/index/deleteAllAlvinIndexBatchJobs.sh
+++ development/index/deleteAllAlvinIndexBatchJobs.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 
 LOGIN_URL='http://localhost:8181/login/rest/apptoken/'
-RUNNING_URL='http://localhost:8181/alvin/rest/record/system'
-RECORDTYPE_URL='http://localhost:8081/alvin/rest/record/indexBatchJob'
+RUNNING_URL='http://localhost:8081/alvin/rest/record/system'
+RECORDLIST_URL='http://localhost:8081/alvin/rest/record/indexBatchJob'
 LOGINID='systemoneAdmin@system.cora.uu.se'
 APP_TOKEN='5d3f3ed4-4931-4924-9faa-8eaf5ac6457e'
 
 #index preview from outside
 #LOGIN_URL='https://cora.alvin-portal.org/login/rest/apptoken/'
 #RUNNING_URL='https://cora.alvin-portal.org/rest/record/system'
-#RECORDTYPE_URL='https://cora.alvin-portal.org/rest/record/indexBatchJob'
+#RECORDLIST_URL='https://cora.alvin-portal.org/rest/record/indexBatchJob'
 
 #index dev from outside
 #LOGIN_URL='http://130.238.171.238:38181/login/rest/apptoken/'
 #RUNNING_URL='http://130.238.171.238:38081/alvin/rest/record/system'
-#RECORDTYPE_URL='http://130.238.171.238:38081/alvin/rest/record/indexBatchJob'
+#RECORDLIST_URL='http://130.238.171.238:38081/alvin/rest/record/indexBatchJob'
 
 start(){
 	importDeleteScript;
--- development/index/deleteAllDiVAIndexBatchJobs.sh
+++ development/index/deleteAllDiVAIndexBatchJobs.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 LOGIN_URL='http://localhost:8182/login/rest/apptoken/'
-RUNNING_URL='http://localhost:8182/diva/rest/record/system'
-RECORDTYPE_URL='http://localhost:8082/diva/rest/record/indexBatchJob'
+RUNNING_URL='http://localhost:8082/diva/rest/record/system'
+RECORDLIST_URL='http://localhost:8082/diva/rest/record/indexBatchJob'
 LOGINID='systemoneAdmin@system.cora.uu.se'
 APP_TOKEN='5d3f3ed4-4931-4924-9faa-8eaf5ac6457e'
 
 #index def from outside
 #LOGIN_URL='http://130.238.171.238:38182/login/rest/apptoken/'
 #RUNNING_URL='http://130.238.171.238:38082/diva/rest/record/system'
-#RECORDTYPE_URL='http://130.238.171.238:38082/diva/rest/record/indexBatchJob'
+#RECORDLIST_URL='http://130.238.171.238:38082/diva/rest/record/indexBatchJob'
 
 start(){
 	importDeleteScript;
