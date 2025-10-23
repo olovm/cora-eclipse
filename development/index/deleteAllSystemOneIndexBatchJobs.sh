07401669bf7d5e01d0910f6171eba9e80f299907
olovm <olov.mckie@ub.uu.se>
2025-10-23 11:59:38 +0000

    minor update delete index batch job

--- development/index/deleteAllSystemOneIndexBatchJobs.sh
+++ development/index/deleteAllSystemOneIndexBatchJobs.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
-LOGIN_URL='http://localhost:8180/login/rest/apptoken/'
 RUNNING_URL='http://localhost:8080/systemone/rest/record/system'
+LOGIN_URL='http://localhost:8180/login/rest/apptoken/'
 RECORDLIST_URL='http://localhost:8080/systemone/rest/record/indexBatchJob'
 LOGINID='systemoneAdmin@system.cora.uu.se'
 APP_TOKEN='5d3f3ed4-4931-4924-9faa-8eaf5ac6457e'
 
 #delete dev from outside
+#RUNNING_URL='http://130.238.171.238:38080/systemone/rest/record/system'
 #LOGIN_URL='http://130.238.171.238:38180/login/rest/apptoken/'
 #RECORDLIST_URL='http://130.238.171.238:38080/systemone/rest/record/indexBatchJob'
 
