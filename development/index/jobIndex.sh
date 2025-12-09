a042bc1ad5790d36e368046a1430b563e6100b5f
olovm <olov.mckie@ub.uu.se>
2025-12-09 09:18:58 +0000

    break out login to its own script (for reuse)

--- development/index/jobIndex.sh
+++ development/index/jobIndex.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#set -euo pipefail
 set -uo pipefail
 
 start() {
+  importLogin
   waitingForListOfSystemToEnsureSystemIsRunning
   echo "Starting indexing process..."
 #  loginUsingAppToken
   loginUsingIdpLogin
   index
   logoutFromCora
+}
+
+importLogin(){
+	source "$(dirname "$0")/login.sh"
 }
 
 waitingForListOfSystemToEnsureSystemIsRunning(){
@@ -18,35 +22,6 @@ waitingForListOfSystemToEnsureSystemIsRunning(){
   done
 
   echo "Application is ready. Running indexing script..."
-}
-
-loginUsingAppToken() {
-  echo "Logging in.."
-  local loginAnswer
-  loginAnswer=$(curl -s -X POST \
-  -H "Content-Type: application/vnd.cora.login" \
-  -k -i "${LOGIN_URL}" \
-  --data "${LOGINID}"$'\n'"${APP_TOKEN}")
-  setTokens "$loginAnswer"
-  echo "Logged in... "
-}
-
-loginUsingIdpLogin() {
-  echo "Logging in.."
-  local loginAnswer
-  loginAnswer=$(curl -s -X GET \
-  -H "accept: application/vnd.cora.authentication+json" \
-  -H "eppn: ${LOGINID}" \
-  -k -i "${IDP_LOGIN_URL}login")
-  setTokens "$loginAnswer"
-  echo "Logged in... "
-}
-  
-setTokens() {
-  local loginAnswer="$1"
-  echo "loginAnser in setTokens: ${loginAnswer}"
-  AUTH_TOKEN=$(echo "${loginAnswer}" | grep -oP '(?<={"name":"token","value":")[^"]+')
-  AUTH_TOKEN_DELETE_URL=$(echo "${loginAnswer}" | grep -oP '(?<="url":")[^"]+')
 }
 
 index() {
@@ -111,13 +86,6 @@ indexData() {
   else
     echo "⚠️  Skipping due to missing required fields"
   fi
-}
-
-logoutFromCora() {
-  echo
-  echo "Logging out from ${AUTH_TOKEN_DELETE_URL}"
-  curl -s -X DELETE -k -H "authToken: ${AUTH_TOKEN}" -i "${AUTH_TOKEN_DELETE_URL}"
-  echo "Logged out"
 }
 
 start
